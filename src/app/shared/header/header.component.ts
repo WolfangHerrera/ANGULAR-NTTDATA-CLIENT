03ac4c88bc7b761a59df84d810bfccca47e7e6b2
wolfangherrera <andreyherrerac@icloud.com>
2024-12-15 23:04:55 -0500

    add exit button to header for user navigation and implement confirmation dialog

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isInUserPath: boolean = false;
 
+  constructor(private readonly router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.checkIfInDashboard();
+    this.router.events.subscribe(() => {
+      this.checkIfInDashboard();
+    });
+  }
+
+  checkIfInDashboard(): void {
+    const currentRoute = this.router.url;
+    this.isInUserPath = currentRoute.includes('user');
+  }
+
+  navigateToHome(): void {
+    Swal.fire({
+      title: 'ARE YOU SURE?',
+      text: 'DO YOU WANT TO GO BACK TO THE MAIN MENU?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'YES, GO BACK!',
+      cancelButtonText: "NO, STAY HERE",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate(['/dashboard']);
+      }
+    });    
+  }
 }
